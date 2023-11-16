59cddc38de93451dfff49dd4a9c738ffbdf87543
Sebastian <sebastiansieradzki99@gmail.com>
2023-11-16 16:30:07 +0100

    add view for student list and modal

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.index, name='index'),
-}
+    path('<int:id>', views.view_student, name='view_student'),
+]
